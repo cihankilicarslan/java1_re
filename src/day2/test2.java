12edecdffdd0ac430a0fafd6c17769191f23b368
Cihan Kilicarslan <cihankilicarslan@hotmail.com>
2022-07-07 19:16:47 -0400

    test

--- src/day2/test2.java
+++ src/day2/test2.java
@@ -4,8 +4,7 @@ public class test2 {
 
     public static void main(String[] args) {
         System.out.println("merhaba2");
-        System.out.println("merhaba2");
-        System.out.println("deneme");
+
 
 
 
